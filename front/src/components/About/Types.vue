0c516b75db3880289cf4629c262c8b805115de16
ryu <munam4@naver.com>
2023-01-02 19:48:50 +0900

    ✨ 네비게이션 바 반응형 햄버거 기능 추가

---
+++ front/src/components/About/Types.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="type-container">
+    <p class="type">TYPE OF WORKATION</p>
+
+    <div class="type1">
+      <img src="@/assets/Home/mindfull.png" />
+      <div class="texts">
+        <p class="header">TYPE #1</p>
+        <p class="card-subtitle">리더들을 위한</p>
+        <p class="card-title">마인드풀 워케이션</p>
+        <p class="card-description">
+          서로에게 영감을 주는 리더들이 모여 여행지에서 새로운 라이프스타일을
+          즐기고 일도 하면서 서로간의 의미있는 연결을 통해 재충전하는 프로그램
+        </p>
+        <p class="card-subdescription">
+          내면탐색, 체형교정 테라피, 네트워킹, 인사이트 토크 등 다양한
+          프로그램을 통해 일과 삶에 대한 영감과 훌륭한 인사이트를 얻을 수 있는
+          기회를 만나보세요.
+        </p>
+        <button>둘러보기</button>
+      </div>
+    </div>
+
+    <div class="type2">
+      <div class="texts">
+        <p class="header">TYPE #2</p>
+        <p class="card-subtitle">2-Weeks & 4-Weeks</p>
+        <p class="card-title">애니웨어 리트릿</p>
+        <p class="card-description">
+          설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명
+        </p>
+        <p class="card-subdescription">
+          설명설명설명설명설명설명설명설명설명설명설명
+          설명설명설명설명설명설명설명설명
+          설명설명설명설명설명설명설명설명설명설명ㅍ
+        </p>
+        <button>둘러보기</button>
+      </div>
+      <img src="@/assets/Home/retlit.png" />
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutTypesVue",
+};
+</script>
+
+<style scoped>
+.type-container {
+  background-color: #fffef6;
+  padding: 79px 0px 0px 0px;
+}
+.type {
+  font-family: "Montserrat";
+  font-weight: 800;
+  font-size: 3rem;
+  color: #ff4e31;
+  margin: 0;
+}
+
+.type1 {
+  display: flex;
+  justify-content: center;
+}
+
+.type2 {
+  display: flex;
+  justify-content: center;
+}
+
+.texts {
+  text-align: left;
+}
+.header {
+  font-family: "Montserrat";
+  font-weight: 600;
+  font-size: 2rem;
+  color: #ff4e31;
+}
+
+.card-subtitle {
+  font-weight: 400;
+  font-size: 2.5rem;
+}
+
+.card-title {
+  font-weight: 700;
+  font-size: 4rem;
+}
+
+.card-description {
+  font-weight: 500;
+  font-size: 2rem;
+}
+
+.card-subdescription {
+  font-weight: 350;
+  font-size: 2rem;
+  color: #666666;
+}
+
+button {
+  margin-left: 10px;
+  margin-right: 10px;
+  color: #FF4E31;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight: bold;
+  padding: 12.5px 33.5px 12.5px 33.5px;
+  border: 1.5px solid #FF4E31;
+  border-radius : 50px;
+  background-color: rgba( 255, 255, 255, 0);
+}
+
+</style>
--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -2,21 +2,25 @@
   <div>
     <Banner/>
     <AboutPurposeVue/>
+    <TypesVue/>
+    
   </div>
 </template>
 
 <script>
 import Banner from '@/components/About/Banner.vue';
 import AboutPurposeVue from '@/components/About/AboutPurpose.vue';
+import TypesVue from '@/components/About/Types.vue';
 export default {
   name: 'AboutView',
   components: {
     Banner,
-    AboutPurposeVue
+    AboutPurposeVue,
+    TypesVue
   }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
