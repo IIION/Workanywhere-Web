9d034db0787efb7e9d63257549523e168bdae053
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2023-01-13 19:12:50 +0900

    ✨ program 페이지의 세그먼트 컨트롤 구성 및 조건 설정

---
+++ front/src/components/Program/ProgramCollection.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="collection-view">
+      <div v-for="(cell, idx) in filteredCells"
+        :key="idx">
+        <div class="cell">{{ cell.title }}</div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProgramCollection",
+    props: {
+        selection: {
+            type: String,
+            default: null
+        },
+        cells: {
+            type: Array,
+            default: () => []
+        }
+    },
+    computed: {
+        filteredCells() {
+            console.log(this.selection)
+            if (this.selection === "ALL") return this.cells;
+            return this.cells.filter(cell => cell.region === this.selection)
+        }
+    }
+}
+</script>
--- front/src/views/ProgramView.vue
+++ front/src/views/ProgramView.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
     <Banner :detail="bannerdetail"/>
+    <segment-control 
+    @selection="onSegmentSelected">
+    </segment-control>
+    <program-collection :selection="selection" :cells="cells">
+    </program-collection>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/About/Banner.vue';
+import SegmentControl from '../components/Program/SegmentControl.vue';
+import ProgramCollection from '../components/Program/ProgramCollection.vue';
 
 export default {
   name: "ProgramView",
   components: {
-    Banner
+    Banner,
+    SegmentControl,
+    ProgramCollection
   },
   data: function () {
     return {
@@ -19,7 +28,30 @@ export default {
         type: "PROGRAMS",
         title: "일상의 단조로움을 벗어나 영감을 깨워줄",
         semititle: "당신을 위한 워케이션"
-      }
+      },
+      selection: 'ALL',
+      cells: [
+        {
+          id: 1,
+          title: '리더들을 위한 마인드풀 워케이션',
+          date: '2022-01-01',
+          region: '속초',
+          tags: ['tag1', 'tag2']
+        },
+        {
+          id: 2,
+          title: '천천히 영월',
+          date: '2023-01-01',
+          region: '영월',
+          tags: ['tag1', 'tag2']
+        }
+      ]
+    }
+  },
+  methods: {
+    onSegmentSelected(segment) {
+      console.log(segment)
+      this.selection = segment;
     }
   },
   mounted() {
