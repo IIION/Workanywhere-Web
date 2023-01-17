9b453169b6f9c1f61d67b45164d8f44a709c2713
jeohong <jeohong59@gmail.com>
2023-01-17 13:26:20 +0900

    Text 정렬

--- front/src/views/ServiceApplyView.vue
+++ front/src/views/ServiceApplyView.vue
@@ -15,9 +15,17 @@
     background-color: #FF4E31;
     height : 266px;
     width : 100%;
+    position: relative;
 }
 
 .infoTitle {
     color: #FFFBE3;
+    position: absolute;
+    font-size: 30px;
+    font-weight: bold;
+    text-align: left;
+    top: 50%;
+    margin-top: -50px;
+    margin-left: 92px;
 }
 </style>
