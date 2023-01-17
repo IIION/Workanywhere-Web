4820ac9bfef2eb69734a366ced539c0d6c149c50
jeohong <jeohong59@gmail.com>
2023-01-17 13:15:39 +0900

    ✨ 대문 이미지의 글자 적용

--- front/src/views/ServiceApplyView.vue
+++ front/src/views/ServiceApplyView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div class="banner" >
+            <h1 class="infoTitle">서비스 소개서를 받기 위해<br/>간단한 정보를 작성해주세요</h1>
         </div>
     </div>
 </template>
@@ -14,5 +15,9 @@
     background-color: #FF4E31;
     height : 266px;
     width : 100%;
+}
+
+.infoTitle {
+    color: #FFFBE3;
 }
 </style>
