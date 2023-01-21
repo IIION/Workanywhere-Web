171a3a2ff259527cda19b465fa337725e77fa5e8
ryu <munam4@naver.com>
2023-01-21 16:35:49 +0900

    ♻♻️ 서비스 소개서 작성 모달 리팩토링 및 연결

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -7,8 +7,11 @@
     <KeysVue/>
     <ReviewVue/>
     <PartnersVue/>
-    <ProposalVue/>
+    <ProposalVue v-on:showServiceApply="showServiceApply"/>
     <FooterVue/>
+    <div v-if="showServiceApplyModal == true" class="modal-background">
+      <ServiceApplyModal class="service-apply-modal" v-on:showServiceApply="showServiceApply"/>
+    </div>
   </div>
 </template>
 
@@ -21,6 +24,7 @@ import KeysVue from '@/components/Home/Keys.vue'
 import ReviewVue from '@/components/Home/Review.vue';
 import PartnersVue from '@/components/Home/Partners.vue';
 import ProposalVue from '@/components/Home/Proposal.vue';
+import ServiceApplyModal from '@/components/Home/ServiceApplyModal.vue';
 import FooterVue from '@/components/Footer.vue'
 
 export default {
@@ -34,7 +38,23 @@ export default {
     ReviewVue,
     PartnersVue,
     ProposalVue,
+    ServiceApplyModal,
     FooterVue
+  },
+  data: function() {
+    return {
+      showServiceApplyModal: false
+    }
+  },
+  methods: {
+    showServiceApply(bool){
+      this.showServiceApplyModal = bool
+      if (this.showServiceApplyModal == true) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "unset";
+      }
+    }
   },
   mounted() {
     window.scrollTo(0, 0)
@@ -43,6 +63,26 @@ export default {
 </script>
 
 <style scoped>
-
+.modal-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(5px);
+  z-index: 1000;
+  overflow-y: auto;
+}
+.service-apply-modal {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 1001;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  box-shadow : rgba(0,0,0,0.5) 0 0 0 9999px;
+  border-radius: 20px;
+  
+}
 
 </style>
